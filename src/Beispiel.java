756de0b404ae904e07c3a69631f55529b6535c0e
vampire161996 <kingoftime1996@gmail.com>
2020-04-07 19:02:48 +0200

    Initial commit

---
+++ src/Beispiel.java
@@ -0,0 +1,6 @@
+public class Beispiel {
+    public static void main(String arg[]) {
+        System.out.println("DucHung.Tran@Student.HTW-Berlin.de");
+        System.out.println("Duc Hung Tran");
+    }
+}
